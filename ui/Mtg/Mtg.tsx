ddf9ae76621a311e3e059ebd0e19f8899292f2e3
Aaron Giordano-Barry <opeologist@gmail.com>
2023-01-07 19:04:03 -0500

    feat: align mtg with modular output

--- ui/Mtg/Mtg.tsx
+++ ui/Mtg/Mtg.tsx
@@ -1,67 +1,71 @@
-"use client";
+import { parse } from "papaparse";
+import { deckApiUrl, decks } from "../../constants/mtg.mjs";
 
-import { useEffect, useState } from "react";
-import { getMtgData } from "../../helpers/getMtgData";
+export default async function Mtg() {
+  // @ts-expect-error
+  const csvs = [];
+  // @ts-expect-error
+  const deckNames = [];
+  // @ts-expect-error
+  const deckSetsArr = [];
+  let computedTotal = 0;
 
-export default function Mtg() {
-  const [csvsArr, setCsvsArr] = useState(null);
-  const [deckNames, setDeckNames] = useState(null);
-  const [deckSets, setDeckSets] = useState(null);
-  const [total, setTotal] = useState(null);
+  await Promise.all(
+    decks.map(async ({ name, id }) => {
+      deckNames.push(name);
 
-  useEffect(() => {
-    const asyncGetCsv = async () => {
-      const { csvs, deckNames } = await getMtgData();
+      const response = await fetch(deckApiUrl`${id}`);
+      const result = await response.text();
 
-      // @ts-expect-error
-      setCsvsArr(csvs);
-      // @ts-expect-error
-      setDeckNames(deckNames);
-    };
-    asyncGetCsv();
-  }, []);
-  useEffect(() => {
-    if (csvsArr !== null && csvsArr !== undefined) {
-      // @ts-expect-error
-      const deckSetsArr = [];
-      let computedTotal = 0;
-      // @ts-expect-error
-      csvsArr.forEach((csv) => {
-        deckSetsArr.push(csv[1][6]);
-        // @ts-expect-error
-        csv.forEach((row, i) => {
-          if (i !== 0) {
-            computedTotal +=
-              row.length > 0 && row[12] ? parseFloat(row[12]) : 0;
-          }
-        });
+      parse(result, {
+        complete({ data }) {
+          csvs.push(data);
+        },
       });
+    })
+  );
+  // @ts-expect-error
+  csvs.forEach((csv) => {
+    deckSetsArr.push(csv[1][6]);
+    // @ts-expect-error
+    csv.forEach((row, i) => {
+      if (i !== 0) {
+        computedTotal += row.length > 0 && row[12] ? parseFloat(row[12]) : 0;
+      }
+    });
+  });
+
+  const organizedDecks = {};
+
+  // @ts-expect-error
+  deckNames.forEach((deckName, i) => {
+    // @ts-expect-error
+    if (organizedDecks[deckName]) {
       // @ts-expect-error
-      setDeckSets(deckSetsArr);
+      organizedDecks[deckName].push(deckSetsArr[i]);
+    } else {
       // @ts-expect-error
-      setTotal(computedTotal.toFixed(2));
+      organizedDecks[deckName] = [deckSetsArr[i]];
     }
-  }, [csvsArr]);
+  });
 
   return (
-    total &&
-    deckNames &&
-    deckSets && (
-      <main>
-        <h1>Total: ${total}</h1>
-        <hr />
-        {/* @ts-expect-error */}
-        {deckNames.map((deckName, i) => (
-          <section key={i}>
-            <h2 key={`${deckName}${i}`}>
-              {/* @ts-expect-error */}
-              {deckName} ({deckSets[i].toUpperCase()})
-            </h2>
+    <>
+      {/* @ts-expect-error */}
+      <h1>Total: ${parseFloat(computedTotal).toFixed(2)}</h1>
+      <hr />
+      {Object.keys(organizedDecks).map((key) => (
+        <section key={key}>
+          <h2>{key}</h2>
+          <ul>
             {/* @ts-expect-error */}
-            <h3>{csvsArr[i][1][5]}</h3>
-          </section>
-        ))}
-      </main>
-    )
+            {[...new Set(organizedDecks[key])].map((set) => {
+              // @ts-expect-error
+              return <li key={set}>{set}</li>;
+            })}
+          </ul>
+        </section>
+      ))}
+    </>
   );
 }
