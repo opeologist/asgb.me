eda51203559282e632b9ec88cb102d351e9c6b4a
Aaron Giordano-Barry <get@asgb.me>
2024-12-27 21:15:07 -0600

    chore: simple styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import styles from "./page.module.css";
+
 export default function Home() {
   return (
-    <main>
-      <h1>ASGB</h1>
+    <main className={styles.main}>
+      <h1 className={styles.h1}>ASGB</h1>
     </main>
   );
 }
