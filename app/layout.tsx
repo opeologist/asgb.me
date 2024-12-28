47a04c4157caee8d23caa96c0b26212928299519
Aaron Giordano-Barry <opeologist@gmail.com>
2024-12-27 21:02:50 -0600

    chore: update to next15

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,26 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Open_Sans } from "next/font/google";
+import type { ReactNode } from "react";
+import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const openSans = Open_Sans({
+  variable: "--font-open-sans",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Aaron Giordano-Barry",
+  description: "Aaron Giordano-Barry's personal website",
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={openSans.variable}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-export default function HomePage() {
+export default function Home() {
   return (
     <main>
       <h1>ASGB</h1>
