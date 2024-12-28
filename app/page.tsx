ef6b41bbe50b2b20f762952deb41875f0adb2869
Aaron Giordano-Barry <get@asgb.me>
2024-12-27 21:15:20 -0600

    chore: remove style

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1 className={styles.h1}>ASGB</h1>
+      <h1>ASGB</h1>
     </main>
   );
 }
