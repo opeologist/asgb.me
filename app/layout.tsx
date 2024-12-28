aadd3920cdf78c7a2f4942820a5a2fbb9e3e2bf9
Aaron Giordano-Barry <get@asgb.me>
2024-12-27 21:28:46 -0600

    chore: add blank favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="data:," />
+      </head>
       <body className={openSans.variable}>{children}</body>
     </html>
   );
